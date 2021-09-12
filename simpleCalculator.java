f12eeaa13b82f20d7c2087384add0725a1532574
CameronBrenton <Cameron.Brenton@keyin.com>
2021-09-12 12:42:34 -0230

    Added a while loop so that the program doesn't stop until you tell it too.

--- simpleCalculator.java
+++ simpleCalculator.java
@@ -3,38 +3,53 @@ import java.util.*;
 public class simpleCalculator {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Simple Calculator Program");
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the first number");
-		int num1 = sc.nextInt();
-		System.out.println("Enter the operator");
-		String operator = sc.next();
-		System.out.println("Enter the second number");
-		int num2 = sc.nextInt();
-		int result;
-		switch (operator){
 
-		case "+":
-			result = num1 + num2;
-			System.out.println("The result is " + result);
-			break;
-		
-		case "-":
-			result = num1 - num2;
-			System.out.println("The result is " + result);
-			break;
-		
-		case "/":
-			result = num1 / num2;
-			System.out.println("The result is " + result);
-			break;
-		
-		case "*":
-			result = num1 * num2;
-			System.out.println("The result is " + result);
-			break;
-		
-		default:
-			System.out.println("Error: Please choose a valid operator");
+		while(true){
+			
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter the first number");
+			int num1 = sc.nextInt();
+			System.out.println("Enter the operator");
+			String operator = sc.next();
+			System.out.println("Enter the second number");
+			int num2 = sc.nextInt();
+			int result;
+			switch (operator){
+
+				case "+":
+					result = num1 + num2;
+					System.out.println("The result is " + result);
+					break;
+				
+				case "-":
+					result = num1 - num2;
+					System.out.println("The result is " + result);
+					break;
+				
+				case "/":
+					result = num1 / num2;
+					System.out.println("The result is " + result);
+					break;
+				
+				case "*":
+					result = num1 * num2;
+					System.out.println("The result is " + result);
+					break;
+				
+				default:
+					System.out.println("Error: Please choose a valid operator");
+			}
+			System.out.println("Do you want to continue? (y/n)");
+			String answer = sc.next();
+			if(answer.equals("n")){
+				System.out.println("Goodbye!");
+				break;
+			}
+			else{
+				continue;
+			}
+				
 		}
+		
 	}
 }
